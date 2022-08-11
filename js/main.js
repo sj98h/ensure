5d1133af194a0302da454ab65327e7e1fcc8e386
sj98h <61384591+sj98h@users.noreply.github.com>
2022-08-11 12:37:37 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,16 +1,37 @@
 $(document).ready(function () {
     $('.mo_menu_bt').click(function () {
-        $('.mo_side').show()
-        $('.mo_menu_bg').show()
-        $('.mo_menu_bg').animate({ opacity: 1 })
-        $('.mo_side').animate({ right: 0 + "vw" }, 500)
+        $('.mo_side').show(0, function () {
+            $('.mo_menu_bg').show()
+            $('.mo_menu_bg').animate({ opacity: 1 }, 300)
+            $('.mo_side').animate({ right: 0 + "vw" }, 300)
+        })
     })
     $('.mo_close').click(function () {
-        $('.mo_side').animate({ right: -50 + "vw" }, 500, function () {
+        $('.mo_side').animate({ right: -50 + "vw" }, 300, function () {
             $(this).hide()
         });
         $('.mo_menu_bg').animate({ opacity: 0 }, 500, function () {
             $(this).hide()
         })
+    })
+    $('.mo_menu_li.claim').children('span').click(function () {
+        $(this).siblings('.mo_sub').slideToggle()
+    })
+    setInterval(function () {
+        if (window.innerWidth > 500) {
+            $('.mo_side').animate({ right: -50 + "vw" }, 0, function () {
+                $(this).hide()
+            });
+            $('.mo_menu_bg').animate({ opacity: 0 }, 0, function () {
+                $(this).hide()
+            })
+        }
+    })
+
+    $('.menu_li.claim').mouseenter(function () {
+        $(this).children('.sub').stop().slideDown(300)
+    })
+    $('.menu_li.claim').mouseleave(function () {
+        $(this).children('.sub').stop().slideUp(300)
     })
 })
